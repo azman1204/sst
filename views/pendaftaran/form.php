520b3186b26c8cb9b0e84536b799b418ca62a125
Jimmy <azman740412@gmail.com>
2018-09-19 11:06:08 +0800

    update pendaftaran form u2

--- views/pendaftaran/form.php
+++ views/pendaftaran/form.php
@@ -1,18 +1,18 @@
 <legend>Pendaftaran Saringan</legend>
 
 <ul class="nav nav-tabs">
-  <li class="nav-item">
-    <a class="nav-link active" href="#">Pendaftaran</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="index.php?r=ujian-saringan/form">Ujian Saringan</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Ujian Pengesahan</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Diagnosis Akhir</a>
-  </li>
+    <li class="nav-item">
+        <a class="nav-link active" href="#">Pendaftaran</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="index.php?r=ujian-saringan/form">Ujian Saringan</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Ujian Pengesahan</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Diagnosis Akhir</a>
+    </li>
 </ul>
 
 <?php
@@ -24,13 +24,52 @@ if (isset($salah)) {
 ?>
 <form method="post" action="index.php?r=pendaftaran/save">
     <div class="row">
-        <div class="col col-sm">Nama :</div>
-        <div class="col col-sm"><input type="text" name="nama" value="<?= $dat->nama ?>"></div>
-        <div class="col col-sm">No KP :</div>
-        <div class="col col-sm"><input type="text" maxlength="12" name="nokp" value="<?= $dat->nokp ?>"></div>
+        <div class="col col-sm-2">Nama</div>
+        <div class="col col-sm"><input type="text" name="nama" value="<?= $dat->nama ?>" class="form-control"></div>
+        <div class="col col-sm-2">No KP</div>
+        <div class="col col-sm"><input type="text" maxlength="12" name="nokp" value="<?= $dat->nokp ?>" class="form-control"></div>
     </div>
-    
+    <div class="row">
+        <div class="col col-sm-2">Kebenaran Bertulis</div>
+        <div class="col col-sm"><input type="text" name="nama" value="<?= $dat->nama ?>" class="form-control"></div>
+        <div class="col col-sm-2">Alamat</div>
+        <div class="col col-sm"><input type="text" maxlength="12" name="nokp" value="<?= $dat->nokp ?>" class="form-control"></div>
+    </div>
+    <div class="row">
+        <div class="col col-sm-2">Tel</div>
+        <div class="col col-sm"><input type="text" name="nama" value="<?= $dat->nama ?>" class="form-control"></div>
+        <div class="col col-sm-2">Jantina</div>
+        <div class="col col-sm"><input type="text" maxlength="12" name="nokp" value="<?= $dat->nokp ?>" class="form-control"></div>
+    </div>
+    <div class="row">
+        <div class="col col-sm-2">Tarikh Lahir</div>
+        <div class="col col-sm"><input type="text" name="nama" value="<?= $dat->nama ?>" class="form-control"></div>
+        <div class="col col-sm-2">Umur</div>
+        <div class="col col-sm"><input type="text" maxlength="12" name="nokp" value="<?= $dat->nokp ?>" class="form-control"></div>
+    </div>
+    <div class="row">
+        <div class="col col-sm-2">Klinik Kesihatan</div>
+        <div class="col col-sm"><input type="text" name="nama" value="<?= $dat->nama ?>" class="form-control"></div>
+        <div class="col col-sm-2">Nama Sekolah</div>
+        <div class="col col-sm"><input type="text" maxlength="12" name="nokp" value="<?= $dat->nokp ?>" class="form-control"></div>
+    </div>
+    <div class="row">
+        <div class="col col-sm-2">Etnik</div>
+        <div class="col col-sm"><input type="text" name="nama" value="<?= $dat->nama ?>" class="form-control"></div>
+        <div class="col col-sm-2">Pecahan Etnik</div>
+        <div class="col col-sm"><input type="text" maxlength="12" name="nokp" value="<?= $dat->nokp ?>" class="form-control"></div>
+    </div>
+    <div class="row">
+        <div class="col col-sm-2"></div>
+        <div class="col col-sm"><input type="submit" value="Simpan" class="btn btn-primary"></div>
+    </div>
+
     <input type="hidden" name="_csrf" value="<?= \Yii::$app->request->csrfToken ?>">
     <input type="hidden" name="id" value="<?= $dat->id ?>"> <!-- PK -->
-    <input type="submit" value="Simpan">
 </form>
+
+<style>
+    .row {
+        margin-top: 5px;
+    }
+</style>
