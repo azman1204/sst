31f8f73edce1f30956970e0d17b2d4e9155203db
Jimmy <azman740412@gmail.com>
2018-10-02 14:35:46 +0800

    scrooler hori

--- views/pendaftaran/list.php
+++ views/pendaftaran/list.php
@@ -1,12 +1,17 @@
 <legend>Senarai Pendaftaran</legend>
 <a href="index.php?r=pendaftaran/form" class="btn btn-success btn-sm">Tambah Rekod</a>
+
+<div id="mylist">
 <table class="table table-bordered table-striped">
     <thead class="thead-dark">
     <tr>
         <th>Bil</th>
+        <th>Tindakan</th>
         <th>Nama</th>
         <th>No KP</th>
-        <th>Tindakan</th>
+        <th>Alamat</th>
+        <th>Tel</th>
+        <th>Jantina</th>
     </tr>
     </thead>
     <?php
@@ -15,14 +20,23 @@
     ?>
     <tr>
         <td><?= $bil++ ?></td>
-        <td><?= $data->nama ?></td>
-        <td><?= $data->nokp ?></td>
         <td>
             <a href="index.php?r=pendaftaran/edit&id=<?= $data->id ?>" class="btn btn-info">Edit</a>
             <a href="index.php?r=pendaftaran/delete&id=<?= $data->id ?>" class="btn btn-danger">Hapus</a>
         </td>
+        <td><?= $data->nama ?></td>
+        <td><?= $data->nokp ?></td>
+        <td><?= $data->alamat ?></td>
+        <td><?= $data->tel ?></td>
+        <td><?= $data->jantina ?></td>
     </tr>
     <?php } ?>
 </table>
+</div>
 
+<style>
+    #mylist {
+        overflow: auto;
+    }
+</style>
 
