6c40dd0d832ac5561dd608427a2d38f569ea3dab
Jimmy <azman740412@gmail.com>
2018-10-03 13:12:51 +0800

    pendaftaran list all column u1

--- views/pendaftaran/list.php
+++ views/pendaftaran/list.php
@@ -4,6 +4,7 @@ use app\models\Klinik;
 use app\models\KumpEtnik;
 use app\models\PecahanEtnik;
 use app\models\Sekolah;
+use app\models\UjianSaringan;
 ?>
 <legend>Senarai Pendaftaran</legend>
 <a href="index.php?r=pendaftaran/form" class="btn btn-success btn-sm">Tambah Rekod</a>
@@ -25,6 +26,14 @@ use app\models\Sekolah;
         <th>Klinik Kesihatan</th>
         <th>Kumpulan Etnik</th>
         <th>Pecahan Etnik</th>
+        <th>Tarikh Ujian</th>
+        <th>HB</th>
+        <th>MCH</th>
+        <th>MCB</th>
+        <th>MCHC</th>
+        <th>RDW</th>
+        <th>RDC</th>
+        <th>Diagnosis Sementara</th>
     </tr>
     </thead>
     <?php
@@ -34,6 +43,11 @@ use app\models\Sekolah;
         $ke = KumpEtnik::findOne($data->kump_etnik);
         $sekolah = Sekolah::findOne($data->id_sekolah);
         $pe = PecahanEtnik::findOne($data->pecahan_etnik);
+        $saringan = UjianSaringan::find()->where(['id_pendaftaran' => $data->id])->one();
+        if (! $saringan) {
+            // maklumat ujian saringan belum disimpan
+            $saringan = new UjianSaringan();
+        }
     ?>
     <tr>
         <td><?= $bil++ ?></td>
@@ -43,16 +57,24 @@ use app\models\Sekolah;
         </td>
         <td><?= $data->nama ?></td>
         <td><?= $data->nokp ?></td>
-        <td><?= $data->kebenaran ?></td>
+        <td><?= $data->kebenaran === 'Y' ? 'YA' : 'TIDAK' ?></td>
         <td><?= $data->alamat ?></td>
         <td><?= $data->tel ?></td>
-        <td><?= $data->jantina ?></td>
+        <td><?= $data->jantina === 'L' ? 'Lelaki' : 'Perempuan' ?></td>
         <td><?= $data->tkh_lahir ?></td>
         <td><?= $data->umur ?></td>
         <td><?= $sekolah->nama ?></td>
         <td><?= $klinik->nama ?></td>
         <td><?= $ke->nama ?></td>
         <td><?= $pe->nama_pecahan ?></td>
+        <td><?= $saringan->tkh_ujian ?></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
     </tr>
     <?php } ?>
 </table>
