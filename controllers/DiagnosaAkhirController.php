b9bc392925a459bf1f79da788f75a6a6d03d3d2a
Jimmy <azman740412@gmail.com>
2019-04-16 14:15:01 +0800

    HQ update

--- controllers/DiagnosaAkhirController.php
+++ controllers/DiagnosaAkhirController.php
@@ -8,6 +8,9 @@ class DiagnosaAkhirController extends \yii\web\Controller {
     public function actionForm() {
         $id_pendaftaran = \Yii::$app->session->get('id_pendaftaran');
         $diag2 = UjianSaringan::find()->where(['id_pendaftaran' => $id_pendaftaran])->one();
+        if (! $diag2) {
+            $diag2 = new UjianSaringan();
+        }
         $catatan = $diag2->catatan2;
         if ($diag2->id_diag_sementara == 1) {
             // normal - semasa ujian saringan
@@ -35,6 +38,10 @@ class DiagnosaAkhirController extends \yii\web\Controller {
         //echo $id_pendaftaran;exit;
         $diag1 = UjianPengesahan::find()->where(['id_pendaftaran' => $id_pendaftaran])->one();
         $diag2 = UjianSaringan::find()->where(['id_pendaftaran' => $id_pendaftaran])->one();
+        if (! $diag2) {
+            $diag2 = new UjianSaringan();
+            $diag2->id_pendaftaran = $id_pendaftaran;
+        }
         $diag2->catatan2 = $catatan;
         $diag2->save();
         if (! $diag1) {
